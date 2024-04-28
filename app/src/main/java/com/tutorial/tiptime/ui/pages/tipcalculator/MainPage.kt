1bb5ad54772b022fbb54fb8910a76244016d580a
Joshua Hansen <jjhansen30@gmail.com>
2024-04-28 12:41:44 -0600

    clean up warnings

--- app/src/main/java/com/tutorial/tiptime/ui/pages/tipcalculator/MainPage.kt
+++ app/src/main/java/com/tutorial/tiptime/ui/pages/tipcalculator/MainPage.kt
@@ -2,10 +2,8 @@ package com.tutorial.tiptime.ui.pages.tipcalculator
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -18,11 +16,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.tutorial.tiptime.R
 
 @OptIn(ExperimentalMaterial3Api::class)
